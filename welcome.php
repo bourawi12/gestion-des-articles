4f825c4ca772e167e0a4449945963f6c5198067f
bourawi12 <mohamed12bouaziz@gmail.com>
2023-10-08 11:34:52 +0100

    Gestion des Articles

---
+++ welcome.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>A Fancy Table</h1>
+<form action="welcome.php" method="post"></form>
+<table id="customers">
+  <tr>
+    <th>Code</th>
+    <th>Name</th>
+    <th>price</th>
+    <th>quantity</th>
+    <th>action</th>
+  </tr>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "store";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT code,name, quantity, price FROM article"; // Remove "code" from the query
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr> <td>: ". $row["code"]. "</td>-<td> name".$row["quantity"]. "</td> -<td> Quantity: ". $row["quantity"]. "</td> -<td> Price: " . $row["price"]. "</td> -<td>".</tr>";
+    }
+} else {
+    echo "0 results";
+}
+
+
+$conn->close();
+?>
+</table>
+
+</body>
+</html>
+
+
+
